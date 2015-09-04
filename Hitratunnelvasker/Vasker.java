ae7c59439cb05b52bc0ed10e2b2b632e703ccf5f
Sinhk <sindre.kroknes@gmail.com>
2015-09-04 10:44:13 +0200

    import code

--- Hitratunnelvasker/Vasker.java
+++ Hitratunnelvasker/Vasker.java
@@ -3,9 +3,35 @@
 *
 * Styreprogram for Hitratunnelvasker
 */
+import lejos.hardware.motor.*;
+import lejos.hardware.lcd.*;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.EV3GyroSensor;
+import lejos.hardware.sensor.NXTTouchSensor;
+import lejos.hardware.sensor.NXTLightSensor;
+import lejos.hardware.sensor.NXTColorSensor;
+import lejos.hardware.sensor.NXTSoundSensor;
+import lejos.hardware.sensor.NXTUltrasonicSensor;
+import lejos.hardware.port.Port;
+import lejos.hardware.Brick;
+import lejos.hardware.BrickFinder;
+import lejos.hardware.ev3.EV3;
+import lejos.hardware.Keys;
+import lejos.hardware.sensor.SensorModes;
+import lejos.robotics.SampleProvider;
+import lejos.hardware.sensor.*;
 
 class Vasker{
-	public static void main(String[] args){
+	public static void main(String[] args) throws exceptions{
+		try{
+		Brick brick = BrickFinder.getDefault();
+    		Port s1 = brick.getPort("S1"); // fargesensor
+    		Port s2 = brick.getPort("S2"); // trykksensor
+    		Port s3 = brick.getPort("S3"); // ultrasonisksensor
+			
+		}	
 		
 		
 		
