0707ad657a2a73c5debe7e5e7c9369d059c9dc84
Sinhk <sindre.kroknes@gmail.com>
2015-11-20 00:08:34 +0100

    Kommentering i BTConnectEV3

--- src/plukker/BTConnectEV3.java
+++ src/plukker/BTConnectEV3.java
@@ -1,23 +1,31 @@
 package plukker;
 
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
 import lejos.remote.nxt.BTConnector;
+import lejos.remote.nxt.NXTConnection;
 import lejos.remote.nxt.BTConnection;
 
+/**
+ * Klasse for bluetooth kommunikasjon på EV3. 
+ * 
+ * @author sindr
+ *
+ */
 public class BTConnectEV3 implements Runnable {
 
     private volatile boolean ready = false;
     private volatile boolean done = false;
     private volatile boolean running;
-    private String address = "00:16:53:0A:57:A7";// Adresse til NXTen
+    
+    // Adresse til NXTen
+    private String address = "00:16:53:0A:57:A7";
+    
     private final int READY = 1;
     private final int DONE = 5;
 
     public void run() {
-	// TODO Add timout on connection, if necesary
 	running = true;
 	boolean connected = false;
 	BTConnector con = new BTConnector();
@@ -25,20 +33,22 @@ public class BTConnectEV3 implements Runnable {
 	// DataInputStream dis = null;
 	DataOutputStream dos = null;
 	while (running) {
-	    /* Try to connect until success, then open io streams */
+	    // Try to connect until success, then open io streams
 	    while (running && nxt == null) {
-		nxt = con.connect(address, BTConnection.PACKET);
+		nxt = con.connect(address, NXTConnection.PACKET);
 	    }
-	    connected = true;
 	    // dis = nxt.openDataInputStream();
 	    dos = nxt.openDataOutputStream();
+	    connected = true;
+	   /*
 	    try {
 		Thread.sleep(300);
 	    } catch (InterruptedException ie) {
 		running = false;
-	    }
+	    }*/
 	    while (running && connected) {
 		try {
+		    //Sender verdi om en boolean er true
 		    int value = 0;
 		    if (ready) {
 			value = READY;
@@ -50,25 +60,24 @@ public class BTConnectEV3 implements Runnable {
 		    }
 		    if (value != 0)
 			dos.writeInt(value);
-		    /* Handle disconnect/ioexception */
-		} catch (IOException ioe) {
-		    System.out.println("IOException writing:");
-		    System.out.println(ioe.getMessage());
+		    
+		    // Behandle disconnect/ioexception
+		    } catch (IOException ioe) {
 		    connected = false;
 		    try {
 			// dis.close();
 			dos.close();
 			Thread.sleep(100);
 			nxt.close();
+			nxt = null;
+			connected = false;
 		    } catch (IOException e) {
 			System.out.println("IOException reconnecting:");
 			System.out.println(ioe.getMessage());
 		    } catch (InterruptedException e) {
 			running = false;
 		    }
-
 		}
-
 	    }
 	}
 	try {
@@ -84,12 +93,16 @@ public class BTConnectEV3 implements Runnable {
 
 	}
     }
-
+    /**
+     * Setter at klarsignal skal sendes
+     */
     public void setReady() {
 	ready = true;
     }
 
-    // Tell thread to stop
+   /**
+    * Setter at avsluttsignal skal sendes
+    */
     public void setDone() {
 	done = true;
     }
