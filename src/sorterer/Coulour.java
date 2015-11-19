37b519657536e658eb84c61c7079e7078e2d43f5
Didirik <Didirik@158.38.209.225>
2015-11-19 14:56:28 +0100

    comment

--- src/sorterer/Coulour.java
+++ src/sorterer/Coulour.java
@@ -14,58 +14,61 @@ import lejos.nxt.addon.*;
 public class Coulour {
     private final static int RED = 0;
     private final static int BLUE = 7;// 2
-    private static float speed = 250;
-    private static float speed2 = 200;
-    private static int vinkel1 = 320;
+    private static float speed = 350;			//setter heishastighet
+    private static float speed2 = 200;			//setter roteringshastighet
+    private static int vinkel1 = 365;			//setter heis rotasjon
     private static int vinkel2 = 30;
-    private static int vinkel3 = 40;
-    private static int vent = 1500;
+    private static int vinkel3 = 60;			// setter sortererens rotasjonsvinkel
+    private static int vent = 1500;				// setter ventetid for hvorlenge sortereren skal vente på at ballen skall lande i båsen sin
     private static int vent2 = 1000;
-    private static NXTRegulatedMotor heis = Motor.A;
-    private static NXTRegulatedMotor inndeler = Motor.B;
-
+    private static NXTRegulatedMotor heis = Motor.A;		//døper motor A til heis
+    private static NXTRegulatedMotor inndeler = Motor.B;	// døper motor B til inndeler
+    
+    
     public static void main(String[] arg) throws Exception {
-	LCD.drawString("Klar...", 0, 0);
+	LCD.drawString("Klar...", 0, 0);						// skriver ut at programmet er klart til å kjøre
 	// Button.waitForAnyPress();
 	BTConnectNXT btc = new BTConnectNXT();
 	Thread btThread = new Thread(btc);
 	btThread.start();
 	heis.resetTachoCount();
 	inndeler.resetTachoCount();
-	heis.setSpeed(speed);
-	inndeler.setSpeed(speed2);
-	ColorHTSensor colorSensor = new ColorHTSensor(SensorPort.S2);
+	heis.setSpeed(speed);									//setter heishastigheten
+	inndeler.setSpeed(speed2);								//setter indeler hastigheten
+	ColorHTSensor colorSensor = new ColorHTSensor(SensorPort.S2);		// forteller NXT I hvilken 
 	// colorSensor.initBlackLevel();
 	boolean fortsett = true;
-
-	while (fortsett) {
+	
+	zero();
+	
+	while (fortsett) {										// starter løkken for sortering og kalibrering 
 	    if (Button.RIGHT.isDown()) {
-		colorSensor.initBlackLevel();
+		colorSensor.initBlackLevel();						// setter kalibrerer til svartnivåer
 	    }
 	    if (Button.LEFT.isDown()) {
-		zero();
+	    colorSensor.initWhiteBalance();						// setter kalibrerer til hvitbalanse
 	    }
 
-	    if (btc.getReady() || Button.ENTER.isDown()) {
+	    if (btc.getReady() || Button.ENTER.isDown()) {		
 		LCD.clear();
 		LCD.drawString("sorterer", 0, 2);
 		int color = colorSensor.getColorID();
 		Color rgb = colorSensor.getColor();
-		LCD.drawString("R: " + rgb.getRed() + " G: " + rgb.getGreen() + " B: " + rgb.getBlue(), 0, 5);
-		LCD.drawString("color" + color, 1, 4);
+		LCD.drawString("R: " + rgb.getRed() + " G: " + rgb.getGreen() + " B: " + rgb.getBlue(), 0, 5);		//skreiver RGB verdien på LCD skjermen
+		LCD.drawString("color" + color, 1, 4);																//skriver ut farge ID
 		sortBall(color);
 	    }
-	    if (btc.getDone() || Button.ESCAPE.isDown()) {
-		fortsett = false;
+	    if (btc.getDone() || Button.ESCAPE.isDown()) {														// stopper løkken om escape knappen blir brukt 
+		fortsett = false;																							
 
 	    }
 	}
-	btc.close();
+	btc.close();																							//stopper bluetooth tilkobling
 	btThread.join();
 	System.exit(0);
     }
 
-    private static void sortBall(int color) {
+    private static void sortBall(int color) {																//sorterer baser på farge
 	switch (color) {
 	case BLUE:
 	    inndeler.rotateTo(vinkel3);
@@ -76,7 +79,7 @@ public class Coulour {
 	default:
 	    break;
 	}
-	heis.rotateTo(vinkel1 + vinkel2);
+	heis.rotateTo(vinkel1 + vinkel2);																		//algoritmen for å returnere heisen til 0 possisjon
 	Delay.msDelay(vent);
 	inndeler.rotateTo(0, true);
 	heis.rotateTo(0);
