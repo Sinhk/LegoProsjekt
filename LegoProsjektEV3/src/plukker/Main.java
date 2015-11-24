b9c2d31138ebc6e526552cd725588d78e3c94976
Sinhk <sindre.kroknes@gmail.com>
2015-11-24 22:30:08 +0100

    Formating

--- LegoProsjektEV3/src/plukker/Main.java
+++ LegoProsjektEV3/src/plukker/Main.java
@@ -5,49 +5,48 @@ import lejos.hardware.Button;
 import lejos.hardware.lcd.LCD;
 
 class Main {
+    public static void main(String[] arg) throws Exception {
+	double speed = 30;
+	float searchRadius = 1f;
+	float ballSize = 5f;
+	Sensor sensor = new Sensor();
+	Pickup pickUp = new Pickup();
+	Mover motor = new Mover(sensor, pickUp, speed);
+	BTConnectEV3 nxt = new BTConnectEV3();
+	Thread btThread = new Thread(nxt);
+	// btThread.start();
+	// motor.testRotate();
+	Radar radar = new Radar(sensor, motor, searchRadius, ballSize);
+	radar.findPoints();
+	LCD.drawString("Press enter to start", 0, 4);
+	Button.ENTER.waitForPressAndRelease();
+	radar.navigate();
 
-	public static void main(String[] arg) throws Exception {
-		double speed = 30;
-		int maxSteer = 160;
-		Sensor sensor = new Sensor();
-		Pickup pickUp = new Pickup();
-		Mover motor = new Mover(sensor,pickUp, speed, maxSteer);
-		BTConnectEV3 nxt = new BTConnectEV3();    
-		Thread btThread = new Thread(nxt);
-		//btThread.start();
-		//motor.testRotate();
-		Radar radar = new Radar(sensor, motor, 1f);
-		radar.makeMap();
-		LCD.drawString("Press enter to start", 0, 4);
-		Button.ENTER.waitForPressAndRelease();
+	// motor.start();
+	// EV3 ev3 = (EV3) BrickFinder.getLocal();
+	boolean fortsett = true;
+	while (fortsett) {
+	    if (sensor.getBall()) {
+		pickUp.pickup();
+		// Delay.msDelay(400);
+		motor.goHome();
+		nxt.setReady();
+		motor.resumeSearch();
+	    }
+	    if (Button.ESCAPE.isDown())
+		fortsett = false;
+	    if (Button.ENTER.isDown()) {
+		motor.setPose0();
+		sensor.resetGyro();
+		radar.findPoints();
 		radar.navigate();
-		
-		//motor.start();
-		//EV3 ev3 = (EV3) BrickFinder.getLocal();
-		boolean fortsett = true;
-		while (fortsett) {
-		    if (sensor.getBall()) {
-			pickUp.pickup();
-			//Delay.msDelay(400);
-			motor.goHome();
-			nxt.setReady();
-			motor.resumeSearch();
-		    }
-			if (Button.ESCAPE.isDown())
-			    fortsett = false;
-			if (Button.ENTER.isDown()){
-			    motor.setPose0();
-			    sensor.resetGyro();
-			    radar.makeMap();
-			    radar.navigate();
-			    }
-			Thread.sleep(50);
-		}
-		motor.terminate();
-		nxt.setDone();
-		motor.join();
-		btThread.join();
-		
-		
+	    }
+	    Thread.sleep(50);
 	}
+	motor.terminate();
+	nxt.setDone();
+	motor.join();
+	btThread.join();
+
+    }
 }
